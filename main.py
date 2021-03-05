6e11561fb091d5ee27c44baf839507a5e425b06b
dlnwlkmn <lesh77906@gmail.com>
2021-03-05 22:40:47 +0300

    Зачатки архитектуры, дочернее окно, выбор файла, отключение

--- main.py
+++ main.py
@@ -1,69 +1,126 @@
-# coding: utf-8
-import threading
-import time
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
 import serial
 import serial.tools.list_ports as port_list
-import tkinter as tk
 
-ports = list(port_list.comports())
-for p in ports:
-    print (p)
+class Connection(tk.Frame):
+    # Конструктор
+    def __init__(self, master=None, title=None):
+        super().__init__(master)
+        self.pack()
+        self.master = master
+        self.master.title(title)
+        self.master.resizable(False, False)
+        self.set_layout()
 
-in_list = []        # список полученных строк
-out_flag = False    # признак занятости вывода и ввода
+    # Функция настройки элементов интерфейса
+    def set_layout(self):
+        # Получаем доступные порты
+        ports = []
+        temp_ports = list(port_list.comports())
+        for port in temp_ports:
+            splited = str(port).split()
+            ports.append(splited[0])
+        ports.reverse()
 
-ser = serial.Serial('COM1', 9600, timeout=1)
+        # Лейба и список портов
+        self.port_list_lable = ttk.Label(self, text="Выбирите доступный порт:")
+        self.port_list_lable.grid(row=0, column=0, sticky=tk.W, padx=10, pady=10)
+        self.port_list = ttk.Combobox(self, values=ports)
+        self.port_list.grid(row=0, column=1, sticky=tk.W+tk.E, padx=10)
 
-# функция приема строки
-def fn_in():
-    print("kkk")
-    global in_list
-    while 1:
-        # ждем прихода к нам строки
-        in_len = 0
-        while in_len < 1:
-            in_str = ser.readline()
-            in_len = len(in_str)
-        # ждем освобождение входного буфера и записываем в него строку
-        in_list.append(in_str)
-        time.sleep(1)
+        # Лейба и строка для ввода скорости работы порта
+        self.port_spd_lable = ttk.Label(self, text="Укажите скорость передачи:")
+        self.port_spd_lable.grid(row=1, column=0, sticky=tk.W, padx=10, pady=4)
+        self.port_spd_entity = ttk.Entry(self)
+        self.port_spd_entity.grid(row=1, column=1, sticky=tk.W+tk.E, padx=10)
 
-# запускаем поток приема
-tr_in = threading.Thread(target=fn_in)
-tr_in.daemon = True
-tr_in.start()
+        # Кнопка подключения
+        self.connect_btn = ttk.Button(self)
+        self.connect_btn["text"] = "Подключиться"
+        self.connect_btn["command"] = self.connect
+        self.connect_btn.grid(row=3, columnspan=3, column=0, sticky=tk.E, padx=10, pady=4)
 
-# запуск основного потока
-def fn_out():
-    global out_flag
-    out_flag = True
+    # Функция подключения к порту
+    def connect(self):
+        try:
+            self.connection = serial.Serial(self.port_list.get(), baudrate=int(self.port_spd_entity.get()), timeout=1)
+        except ValueError as e:
+            errHandler(e)
+        except serial.SerialException as e:
+            errHandler(e)
+        else:
+            print(self.connection)
+            # Сохраняем порт и вызываем дочернее окно
+            self.portVar = tk.StringVar()
+            self.portVar.set(self.port_list.get())
+            Connected(master=tk.Toplevel(), parent=self)
 
-# функция отправки
-def fn_send():
-    out_str = ed.get()
-    if len(out_str) > 0:
-        ser.write(out_str)
-        lb.insert(tk.END, ">>>"+out_str)
-    ed.delete(0, tk.END)
+# Дочернее окно (после совершения подключения)
+class Connected(tk.ttk.Frame):
+    # Конструктор
+    def __init__(self, master=None, parent = None):
+        super().__init__(master)
+        self.parent = parent
+        self.pack()
+        self.master.title(self.parent.master.title()+f" ({self.parent.portVar.get()})")
+        self.master.geometry('550x550')
+        self.master.resizable(False, False)
+        self.parent.master.withdraw()
+        self.filename = tk.StringVar()
+        self.filename.set("Файл не выбран")
+        self.set_layout()
 
-def fn_disp():
-    global out_flag
-    while len(in_list) > 0:
-        str = in_list.pop(0)
-        lb.insert(tk.END, str)
-    if out_flag:
-        fn_send()
-        out_flag = False
-    root.after(100, fn_disp())
+    # Функция настройки элементов интерфейса
+    def set_layout(self):
+        # Лейба выбора файла
+        self.file_lable = ttk.Label(self, text="Выбирите файл:")
+        self.file_lable.grid(row=0, column=0, sticky=tk.W, padx=10, pady=10)
 
-root = tk.Tk()
-entry_var = tk.StringVar(root)
-lb = tk.Listbox(root, width=20, height=5).pack()
-ed = tk.Entry(root, width=20, textvariable=entry_var).pack()
-btn = tk.Button(root, text="Send", width=20, command=fn_out()).pack()
-out_flag = False
-root.after(10, fn_disp())
+        # Кнопка отключения
+        self.disconnect_btn = tk.ttk.Button(self, text = "Отключиться", command = self.disconnect)
+        self.disconnect_btn.grid(row=0, column=1, sticky=tk.E, padx=10, pady=10)
 
-root.mainloop()
+        # Кнопка выбора файла
+        self.pick_file_btn = tk.ttk.Button(self, text="Обзор", command=self.pick_file)
+        self.pick_file_btn.grid(row=1, column=0, sticky=tk.W, padx=10, pady=10)
 
+        # Лейба названия файла
+        self.file_name_lable = ttk.Label(self, text=self.filename.get())
+        self.file_name_lable.grid(row=1, column=1, sticky=tk.W, padx=10, pady=10)
 
+    # Функция выбора файла
+    def pick_file(self):
+        # сохраняем путь до файла
+        self.pwd = fd.askopenfilename()
+        # вынимаем название файла
+        self.temp = self.pwd.split('/')
+        self.filename.set(self.temp[-1])
+        # устанавливаем название выбранного фала лейбе
+        self.file_name_lable.config(text=self.filename.get())
+
+    def disconnect(self):
+        # закрываем подключение к com порту
+        self.parent.connection.close()
+        # проявляем родительское окно
+        self.parent.master.deiconify()
+        # закрываем текущее окно
+        self.master.destroy()
+
+# Функция вывода ошибок в консоль
+def errHandler(e = None):
+    print(f"\033[31mExcepted Error Description:\033[0m\n{e}")
+
+# Главные окна
+root1 = tk.Tk()
+root2 = tk.Tk()
+
+# Начало программы
+if __name__ == '__main__':
+    root1.geometry(f'350x100+{int(root1.winfo_screenwidth()/2)-100}-{int(root1.winfo_screenheight()/2)+50}')
+    root2.geometry(f'350x100+{int(root1.winfo_screenwidth()/2)}-{int(root1.winfo_screenheight()/2)}')
+    app1 = Connection(master=root1, title="Станция 1")
+    app2 = Connection(master=root2, title="Станция 2")
+    app1.mainloop()
+    app2.mainloop()
