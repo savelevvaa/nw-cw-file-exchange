e84621f46e00a156bdc79f89aadb0ac4d3d62275
dlnwlkmn <lesh77906@gmail.com>
2021-03-08 02:13:28 +0300

    minor

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
-# coding: utf-8
+# coding: cp1251
 import tkinter as tk
 from tkinter import ttk
 import serial
 import serial.tools.list_ports as port_list
 import struct
 
+ser = serial.Serial("COM1")
+ser.isOpen()
 
 f_str = open('text.txt', 'rt').read()
 f_bin = open('text.txt', 'rb').read()
