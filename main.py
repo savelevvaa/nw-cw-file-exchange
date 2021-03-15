c921c1a1aba1495cdc7e6c33c8fa0b5f77107025
dlnwlkmn <lesh77906@gmail.com>
2021-03-15 20:09:17 +0300

    Добавлено поле ввода имени пользователя при входе + более точное установление подлючения. Добавлен текст бокс с подлюченными пользователями

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import serial
 import serial.tools.list_ports as port_list
 import threading
 import time
+from network.session import *
 
 class Connection(tk.Frame):
     # Конструктор
@@ -26,60 +27,119 @@ class Connection(tk.Frame):
             ports.append(splited[0])
         ports.reverse()
 
+        # Лейба и строка для ввода Пользователя
+        self.user_lable = ttk.Label(self, text="Пользователь")
+        self.user_lable.grid(row=0, column=0, sticky=tk.W, padx=10, pady=14)
+        self.user_entity = ttk.Entry(self)
+        self.user_entity.insert(tk.END, "pc1")
+        self.user_entity.grid(row=0, column=1, sticky=tk.W + tk.E, padx=10)
+
+        # Разделитель
+        self.separator = tk.ttk.Separator(self)
+        self.separator.grid(row=1, column=0, columnspan=2, sticky=tk.W + tk.E)
+
         # Лейба и список портов
         self.port_list_lable = ttk.Label(self, text="Выбирите доступный порт:")
-        self.port_list_lable.grid(row=0, column=0, sticky=tk.W, padx=10, pady=10)
+        self.port_list_lable.grid(row=2, column=0, sticky=tk.W, padx=10, pady=10)
         self.port_list = ttk.Combobox(self, values=ports)
-        self.port_list.grid(row=0, column=1, sticky=tk.W+tk.E, padx=10)
+        self.port_list.grid(row=2, column=1, sticky=tk.W+tk.E, padx=10)
 
         # Лейба и строка для ввода скорости работы порта
-        self.port_spd_lable = ttk.Label(self, text="Укажите скорость передачи:")
-        self.port_spd_lable.grid(row=1, column=0, sticky=tk.W, padx=10, pady=4)
+        self.port_spd_lable = ttk.Label(self, text="Скорость:")
+        self.port_spd_lable.grid(row=3, column=0, sticky=tk.W, padx=10, pady=4)
         self.port_spd_entity = ttk.Entry(self)
         self.port_spd_entity.insert(tk.END, "9600")
-        self.port_spd_entity.grid(row=1, column=1, sticky=tk.W+tk.E, padx=10)
+        self.port_spd_entity.grid(row=3, column=1, sticky=tk.W+tk.E, padx=10)
+
+        # Лейба и строка для ввода битов данных
+        self.bytesize_lable = ttk.Label(self, text="Биты данных:")
+        self.bytesize_lable.grid(row=4, column=0, sticky=tk.W, padx=10, pady=4)
+        self.bytesize_entity = ttk.Entry(self)
+        self.bytesize_entity.insert(tk.END, "8")
+        self.bytesize_entity.grid(row=4, column=1, sticky=tk.W + tk.E, padx=10)
+
+        # Лейба и строка для ввода стоп-битов
+        self.stopbit_lable = ttk.Label(self, text="Стоп-биты:")
+        self.stopbit_lable.grid(row=5, column=0, sticky=tk.W, padx=10, pady=4)
+        self.stopbit_entity = ttk.Entry(self)
+        self.stopbit_entity.insert(tk.END, "1")
+        self.stopbit_entity.grid(row=5, column=1, sticky=tk.W + tk.E, padx=10)
+
+        # Лейба и строка для ввода таймаута
+        self.timeout_lable = ttk.Label(self, text="Таймаут:")
+        self.timeout_lable.grid(row=6, column=0, sticky=tk.W, padx=10, pady=4)
+        self.timeout_entity = ttk.Entry(self)
+        self.timeout_entity.insert(tk.END, "0.5")
+        self.timeout_entity.grid(row=6, column=1, sticky=tk.W + tk.E, padx=10)
 
         # Кнопка подключения
         self.connect_btn = ttk.Button(self)
         self.connect_btn["text"] = "Подключиться"
         self.connect_btn["command"] = self.connect
-        self.connect_btn.grid(row=3, columnspan=3, column=0, sticky=tk.E, padx=10, pady=4)
+        self.connect_btn.grid(row=8, columnspan=3, column=0, sticky=tk.E, padx=10, pady=4)
 
     # Функция подключения к порту
     def connect(self):
         try:
-            self.connection = serial.Serial(port=self.port_list.get(), baudrate=int(self.port_spd_entity.get()), timeout=0.5)
+            self.connection = serial.Serial(port=self.port_list.get(),
+                                            baudrate=int(self.port_spd_entity.get()),
+                                            bytesize=int(self.bytesize_entity.get()),
+                                            stopbits=int(self.stopbit_entity.get()),
+                                            timeout=float(self.timeout_entity.get()))
         except ValueError as e:
             errHandler(e)
         except serial.SerialException as e:
             errHandler(e)
-        else:
+        finally:
             print(self.connection)
-            # Сохраняем порт и вызываем дочернее окно
-            self.portVar = tk.StringVar()
-            self.portVar.set(self.port_list.get())
-            Connected(master=tk.Toplevel(), parent=self)
+            session = Session(username=self.user_entity.get(), con=self.connection)
+            sessions[session.username] = session.con
+            Connected(master=tk.Toplevel(),
+                      parent=self,
+                      session=session)
 
 # Дочернее окно (после совершения подключения)
 class Connected(tk.ttk.Frame):
     # Конструктор
-    def __init__(self, master=None, parent = None):
+    def __init__(self, master=None, parent = None, session=None):
         super().__init__(master)
         self.parent = parent
         self.pack()
-        self.master.title(self.parent.master.title()+f" ({self.parent.portVar.get()})")
+        self.session = session
+        self.master.title(self.parent.master.title()+
+                          f" ({self.session.username}, "
+                          f"{self.session.con.port}, "
+                          f"{self.session.con.baudrate})")
         self.master.resizable(False, False)
         self.parent.master.withdraw()
         self.filename = tk.StringVar()
         self.filename.set("Файл не выбран")
         self.set_layout()
         self.files = dict()
+
         self.tr_in = threading.Thread(target=self.istream)
         self.tr_in.daemon = True
         self.tr_in.start()
 
+        self.tr_users = threading.Thread(target=self.users)
+        self.tr_users.daemon = True
+        self.tr_users.start()
+
     # Функция настройки элементов интерфейса
     def set_layout(self):
+
+        # Фрейм для лейбы и пользователей
+        self.entryFrame = tk.Frame(self, width=130)
+        self.entryFrame.grid(row=0, rowspan=4, column=4)
+
+        # Лейба пользователей
+        self.session_lable = ttk.Label(self.entryFrame, text="Пользователи:")
+        self.session_lable.grid(row=0, column=0, sticky=tk.W, padx=10, pady=10)
+
+        # Текстбокс для пользователей
+        self.textbox = tk.Text(self.entryFrame, height=5, width=13)
+        self.textbox.grid(row=1, column=0, padx=10, pady=10)
+
         # Лейба выбора файла
         self.file_lable = ttk.Label(self, text="Выбирите файл:")
         self.file_lable.grid(row=0, column=0, sticky=tk.W, padx=10, pady=10)
@@ -165,6 +225,8 @@ class Connected(tk.ttk.Frame):
 
     # Функция отключения
     def disconnect(self):
+        # закрываем сессию
+        sessions.pop(self.session.username)
         # закрываем подключение к com порту
         self.parent.connection.close()
         # проявляем родительское окно
@@ -202,6 +264,19 @@ class Connected(tk.ttk.Frame):
                 # обновляем выпадающий список полученных файлов
                 self.files_list.config(values=keys)
 
+    # Потоковая функция обновления пользователей
+    def users(self):
+        while 1:
+            users = ""
+            users_list = sessions.keys()
+            for user in users_list:
+                users += "* " + user + f" ({sessions[user].port})\n"
+            self.textbox.config(state=tk.NORMAL)
+            self.textbox.delete('1.0', tk.END)
+            self.textbox.insert(tk.INSERT, users)
+            self.textbox.config(state=tk.DISABLED)
+            time.sleep(1)
+
 
 # Функция вывода ошибок в консоль
 def errHandler(e = None):
@@ -211,38 +286,13 @@ def errHandler(e = None):
 root1 = tk.Tk()
 root2 = tk.Tk()
 
+sessions = dict()
+
 # Начало программы
 if __name__ == '__main__':
-    root1.geometry(f'350x100+{int(root1.winfo_screenwidth()/2)-400}-{int(root1.winfo_screenheight()/2)}')
-    root2.geometry(f'350x100+{int(root1.winfo_screenwidth()/2)}-{int(root1.winfo_screenheight()/2)}')
+    root1.geometry(f'350x237+{int(root1.winfo_screenwidth()/2)-400}-{int(root1.winfo_screenheight()/2)}')
+    root2.geometry(f'350x237+{int(root1.winfo_screenwidth()/2)}-{int(root1.winfo_screenheight()/2)}')
     app1 = Connection(master=root1, title="Станция 1")
     app2 = Connection(master=root2, title="Станция 2")
     app1.mainloop()
     app2.mainloop()
-
-
-
-# # Потоковая функция на прием из com-порта
-#     # TODO: Переделать логику работы на словарях (для получения множества файлов)
-#     def istream(self):
-#         in_str = ""
-#         flag = False
-#         while 1:
-#             # ждем прихода к нам строки
-#             in_len = 0
-#             while in_len < 1:
-#                 in_str = self.parent.connection.readline()
-#                 in_len = len(in_str)
-#                 # устанавливаем название полученного файла лейбе
-#                 if self.in_list.__len__() > 1 and flag == False:
-#                     bin_file_name = self.in_list[0]
-#                     self.recieved_file_name = bin_file_name.decode()
-#                     self.file_recieved_lable.config(text="Получен файл: " + self.recieved_file_name)
-#                     self.file_recieved_lable.config(fg="green")
-#                     self.in_list.pop(0)
-#                     flag = True
-#
-#             # ждем освобождение входного буфера и записываем в него строку
-#             self.in_list.append(in_str)
-#             if self.in_list.__len__() > 1:
-#                 self.save_file_btn.config(state="enable")
